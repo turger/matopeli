b7ce47dc0b3a9d558aec7764f1cfe80b0a8ea472
Taru Airola <taru.airola@gmail.com>
2021-01-06 17:18:00 +0200

    Muokkaa create-react-app pohja omalle projektille sopivaksi

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  const headerElement = screen.getByText(/matopeli/i)
+  expect(headerElement).toBeInTheDocument()
+})
